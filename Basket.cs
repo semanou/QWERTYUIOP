51ae23f7624f9478affd60b9b69c74ef3f31c88e
Gansonre Ismael <ismael.gansonre@uqtr.ca>
2021-10-10 21:00:39 -0400

    QWERTYUIOP

---
+++ Basket.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace HarryPotter
+{
+    public class Basket
+    {
+        private readonly IList<Book> _books;
+        public Basket(IEnumerable<Book> books)
+        {
+            _books = (IList<Book>) books.ToList();
+        }
+
+        public int HowMany(Book b)
+        {
+            return  _books.Count(x => x == b);
+
+        }
+
+        public int Count()
+        {
+            var x = _books.Count;
+            return x;
+
+        }
+
+        public int HowManyDifferent()
+        {
+            return _books.Distinct().Count();
+        }
+
+        public  bool IsEmpty()
+        {
+            return !(_books != null && _books.Any());
+            
+
+
+        }
+
+        public Basket RemoveDifferent(int nbToRemove)
+        {
+            var distinctData =  this._books.Distinct();
+            var sortedData = distinctData.OrderBy(x => HowMany(x));
+            var booksToRemove = sortedData.Reverse().Take(nbToRemove);
+            var booksData = _books.ToList(); // Get IEnumerable as List
+
+            foreach (var item in booksToRemove)
+            {
+
+
+                booksData.Remove(_books.First(x => x.Equals(item))); // Remove item
+            }
+            return new Basket(booksData);
+        }
+    }
+}
---
+++ Discount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter
+{
+    public class Discount
+    {
+        private int Nb;
+        private double Ratio;
+
+        public Discount(int nb, double price)
+        {
+            this.Nb = nb;
+            this.Ratio = price;
+            
+        }
+
+        public bool CanBeApplied(Basket b)
+        {
+            return b.HowManyDifferent() >= Nb;
+
+        }
+
+        public double Apply(double basePrice)
+        {
+            return basePrice * (Nb) * Ratio;
+        }
+
+        public Basket RemovePaidBooks(Basket b)
+        {
+            return b.RemoveDifferent(this.Nb);
+        }
+    }
+}
